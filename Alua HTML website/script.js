48a8250750f2b5c7a87f00b09d7a87b09880ef29
aluaburkitbayeva <aluaburkitbayeva2005@gmail.com>
2024-09-26 22:11:30 +0400

    summary

---
+++ Alua HTML website/script.js
@@ -0,0 +1,4 @@
+let insideOfDonut = document.getElementById("Layer_5")
+insideOfDonut.addEventListener("click", () => {
+    document.getElementById("texbox").innerText = "You have clicked on the inside of the donut!"
+})
